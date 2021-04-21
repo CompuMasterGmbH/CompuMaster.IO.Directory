841739c2b1d4d2393f694747c087ac3058999ca0
Thomas Schädler <tschaedler@compuamster.de>
2021-04-21 15:28:57 +0200

    clean up
    removed unnecessary files

--- nuget-build-and-push.cmd
+++
@@ -1,3 +0,0 @@
-::REM -UpdateNuGetExecutable not required since it's updated by VS.NET mechanisms
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'CompuMaster.IO.Directory\_CreateNewNuGetPackage\DoNotModify\New-NuGetPackage.ps1' -ProjectFilePath '.\CompuMaster.IO.Directory\CompuMaster.IO.Directory.vbproj' -verbose -NoPrompt -DoNotUpdateNuSpecFile -PushPackageToNuGetGallery"
-pause
